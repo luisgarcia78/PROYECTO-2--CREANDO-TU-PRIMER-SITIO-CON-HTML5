667eca86689f8caef50327a88a678b40758fd607
luisgarcia78 <89111126+luisgarcia78@users.noreply.github.com>
2021-09-15 19:29:15 -0400

    pagina web proyecto 2
    pagina responsive de php , con informacion basica primera version

---
+++ PROYECTO 2 -CREANDO TU PRIMER SITIO CON HTML5/script.js
@@ -0,0 +1,13 @@
+jQuery('document').ready(function($){
+var menubtn = $('.menu-icon'),
+menu = $('.navigation ul');
+menubtn.click(function(){
+  if (menu.hasClass('show')) {
+    menu.removeClass('show');
+  }else {
+      menu.addClass('show');
+  }
+
+
+  });
+});
